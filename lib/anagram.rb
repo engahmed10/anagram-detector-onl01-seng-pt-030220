3b3bb957360383a57d2d269ab83bd64ced5907fb
AHMED AFANDY <eng.computer1@yahoo.com>
2020-03-21 22:32:47 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,12 +6,12 @@ class Anagram
    def initialize(word)
        @word =word
    end
-
+  aa=[]
    def match(array_of_words)
      word1=@word.split('').sort
      array_of_words.each do |i|
         if i.split('').sort == word1
-          return   i
+          return  a << i
         end
     end
 
