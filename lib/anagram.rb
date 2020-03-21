c6048d0122652ec4c6306c51ff643ed9e0ddf5df
AHMED AFANDY <eng.computer1@yahoo.com>
2020-03-21 22:27:14 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,7 +11,7 @@ class Anagram
      word1=@word.split('').sort
      array_of_words.each do |i|
         if i.split('').sort == word1
-          return  i
+          return  word1
         end
     end
 
