04282592f8dce6976247e778ff3cb433ba1227ec
AHMED AFANDY <eng.computer1@yahoo.com>
2020-03-21 22:02:17 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+
+   attr_accessor :word
+
+   def initialize(word)
+       @word =word
+   end
+
+   def match(array_of_words)
+     @word=@word.split('').sort
+     #binding.pry
+     array_of_words.each do |i|
+
+      if  i.split('').sort == @word
+        #binding.pry
+      end
+     end
+   end
+
+
+
+end
